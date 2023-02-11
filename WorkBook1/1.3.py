f695c154ce0d2be95a35fec8aa3f74ee6326e81b
PTSVU <ptsvuz@gmail.com>
2023-02-11 18:41:37 +0300

    WorkBook1
    1 рабочая тетрадь добавлена, но не доделана

---
+++ WorkBook1/1.3.py
@@ -0,0 +1,17 @@
+X = 5 >= 2
+A = {1, 3, 7, 8}
+B = {2, 4, 5, 10, 'apple'}
+C = A & B
+df = 'Антоновна Антонина', 34, 'ж'
+Z = 'type'
+D = [1, 'title', 2, 'content']
+
+
+print(X, type(X), '\n',
+      A, type(A), '\n',
+      B, type(B), '\n',
+      C, type(C), '\n',
+      df, type(df), '\n',
+      Z, type(Z), '\n',
+      D, type(D), '\n')
+#%%
